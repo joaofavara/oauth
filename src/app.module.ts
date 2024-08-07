14a67e566f085500a6c4be8a7f1e6fbcc233056b
Joao Favara <joaopedro_favara@hotmail.com>
2024-08-07 17:53:58 -0300

    feat: add configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './configuration/configuration';
 
 @Module({
   imports: [
     AuthModule,
-    UsersModule
+    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
   ],
   controllers: [AppController],
 })
